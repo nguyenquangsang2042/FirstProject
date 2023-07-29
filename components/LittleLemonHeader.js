c1ec75c9884ecabe2af05cb4c2d58d971de4cc51
sangnq2903 <nguyenquangsang2042@gmail.com>
2023-07-29 21:19:16 +0700

    a

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -3,8 +3,8 @@ import { View, Text } from 'react-native';
 
 export default function LittleLemonHeader() {
   return (
-    <View style={{ flex: 0.1, backgroundColor: '#F4CE14',justifyContent:'center',alignItems:'center' }}>
-      <Text style={{fontSize:24,fontWeight:'bold'}}>Little lemon</Text>
+    <View style={{ flex: 0.1, backgroundColor: '#F4CE14', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Little lemon</Text>
     </View>
   );
 }
